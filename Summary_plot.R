c5d3b514346bc555c89937c0dfc3140b4b4cb28c
falko-buschke <falko-buschke@users.noreply.github.com>
2024-10-11 16:19:40 +0200

    Update Summary_plot.R
    Updated to consider the new PAVIS v0.2

--- Summary_plot.R
+++ Summary_plot.R
@@ -3,7 +3,7 @@ library(maps)
 library(RColorBrewer)
 
 # Read the data from the cleaned PAVIS Database
-pavis <- read.csv("PAVIS_v0.1.csv", na.strings = "NULL")
+pavis <- read.csv("PAVIS_v0.2.csv", na.strings = "NULL")
 
 # Create a new plot device
 png(filename="Composite.png",width=28,height=28,units="cm",res=300)
@@ -22,7 +22,7 @@ layout(mat = m)
 par(mai=c(0.85,0.65,0.3,0.10))
 
 # Histogram of visitation record per year
-hist(pavis$Year, main="", breaks=seq(1970, 2024,by=1),border="white",col="darkgrey",
+hist(pavis$Year, main="", breaks=seq(1965, 2024,by=1),border="white",col="darkgrey",
      ylim=c(0,180),las=1,xlab="Year", ylab="Number of visitation records",   cex.axis=1.1, cex.lab= 1.5, mgp=c(3.2,0.6,0))
 
 # Plot label
@@ -38,7 +38,7 @@ mtext("a",cex=1.4, font=2,side = 3, adj = -0.1, line = 0.5)
 par(mai=c(0.85,0.65,0.3,0.10))
 
 # Histogram of visitation records 
-hist(table(pavis$WDPAID), breaks=40, xlim=c(0,30),border="white",col="darkgrey",main="", las=1,ylim=c(0,60),
+hist(table(pavis$WDPAID), breaks=40, xlim=c(0,45),border="white",col="darkgrey",main="", las=1,ylim=c(0,60),
      xlab="Number of annual visitation records", ylab="Number of protected areas",   cex.axis=1.1, cex.lab= 1.5, mgp=c(3.2,0.6,0))
 
 # Add a vertical line showing >5 records (these are the data plotted in panel F)
@@ -85,7 +85,7 @@ options(scipen=5)
 par(mai=c(0.85,0.65,0.3,0.10))
 
 # Make plot of annual visitation vs. surface area of protected area (both axes on log-scale)
-plot(meta.dat.clean$REP_AREA,visit.ave, log="xy", las=1,pch=16, col=rgb(0.25,0.25,0.25,0.65),
+plot(meta.dat.clean$REP_AREA,visit.ave, log="xy", las=1,pch=16, col=rgb(0.25,0.25,0.25,0.5),
      xlab=expression("Surface area (km"^ 2*")"), ylab="Average annual visitors",cex=1.8,   
      cex.axis=1.1, cex.lab= 1.5, mgp=c(3.2,0.6,0), bty="n")
 
@@ -154,11 +154,11 @@ map("world", fill=T, col="lightgrey", lwd=1, border="white",
     xlim=c(-20,55), ylim=c(-40,40))
 
 # Add points of protected area centroids, scaled by the average annual visitation
-points(meta.dat.clean$Longitude, meta.dat.clean$Latitude,  cex=log(visit.ave+5)/3,pch=21, 
+points(meta.dat.clean$Longitude, meta.dat.clean$Latitude,  cex=log(visit.ave+5)/6,pch=21, 
        bg=rgb(0.2,0.2,0.8,0.2), col=rgb(0.2,0.2,0.2,1), lwd=0.8)
 
 # Add legend
-legend(-15,-10, pch=21, pt.cex=log(c(10000,1000,100,10))/3, c("10000","1000","100","10"),
+legend(-15,-10, pch=21, pt.cex=log(c(100000,10000,1000,100,10))/6, c("100000","10000","1000","100","10"),
 pt.bg=rgb(0.2,0.2,0.8,0.2), col=rgb(0.2,0.2,0.2,1), title="Mean annual\nvisitors",bty="n")
 
 # Label plot
@@ -173,7 +173,7 @@ mtext("e",cex=1.4, font=2,side = 3, adj = 0.05, line = 0.5)
 # Create a blank vector for standardized growth rates
 growth.rate <- rep(NA,dim(visit.dat)[1] )
 # A vector with year information 
-yrs <- 1974:2023
+yrs <- 1964:2023
 
 # Run a loop to calcualte growth rates
 for (i in 1: dim(visit.dat)[1]){
@@ -194,11 +194,11 @@ map("world", fill=T, col="lightgrey", lwd=1, border="white",
 
 # Add points with centroids of protected areas, coloured by direction adn scale by size of growth rate
 points(meta.dat.clean$Longitude, meta.dat.clean$Latitude,  cex=abs(growth.rate)*20,pch=21, 
-       col=rgb(0.2,0.2,0.2,1), bg=ifelse(growth.rate>=0,rgb(0,0,0.8,0.4),rgb(0.8,0,0,0.4)), lwd=0.8)
+       col=rgb(0.2,0.2,0.2,1), bg=ifelse(growth.rate>=0,rgb(0,0.6,0,0.4),rgb(0.8,0,0,0.4)), lwd=0.8)
 
 # Add legend
 legend(-15,-10, pch=21, pt.cex=c(0.15,0.1,0.05,0.05,0.1,0.15)*20, c("0.15","0.1","0.05","-0.05","-0.1", "-0.15"),
-       pt.bg=c(rgb(0,0,0.8,0.4),rgb(0,0,0.8,0.4),rgb(0,0,0.8,0.4), rgb(0.8,0,0,0.4),rgb(0.8,0,0,0.4),rgb(0.8,0,0,0.4))
+       pt.bg=c(rgb(0,0.6,0,0.4),rgb(0,0.6,0,0.4),rgb(0,0.6,0,0.4), rgb(0.8,0,0,0.4),rgb(0.8,0,0,0.4),rgb(0.8,0,0,0.4))
        , col=rgb(0.2,0.2,0.2,1), title="Growth Rate\n(>5 counts)",bty="n")
 
 # Label panel
@@ -220,7 +220,7 @@ GOV_TYPE <- factor(meta.dat.clean$GOV_TYPE , levels=rev(c("Federal or national m
                                                           "Individual landowners", "Local communities", "Not Applicable", "Not Reported")))
 
 # Creat box plot
-boxplot(visit.ave+1~GOV_TYPE, outline=F, frame=F, horizontal=T,las=1, ylim=c(0,60000),log="",lty=1,ylab="", 
+boxplot(visit.ave+1~GOV_TYPE, outline=F, frame=F, horizontal=T,las=1, ylim=c(0,80000),log="",lty=1,ylab="", 
         cex.axis=1.1, cex.lab= 1.5, mgp=c(3.2,0.6,0),xlab= "Average annual visitors", col="orange")
 
 # Add points for individual protected areas
@@ -266,4 +266,5 @@ mtext("h",cex=1.4, font=2,side = 3, adj = -0.1, line = 0.5)
 dev.off()
 
 #############################################################################################
+
 
